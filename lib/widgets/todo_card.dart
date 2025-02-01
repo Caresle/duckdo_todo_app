ec068358c8e9c229e71a012a58629da66b57f4b2
Caresle <carabesle@gmail.com>
2025-01-31 20:30:50 -0600

    feat(widget): add new styles and bottom sheet in todo card

--- lib/widgets/todo_card.dart
+++ lib/widgets/todo_card.dart
@@ -12,52 +12,86 @@ class TodoCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final backgroundColor = !todo.completed ? Colors.white : Colors.black;
     final textColor = !todo.completed ? Colors.black : Colors.white;
+    final size = MediaQuery.of(context).size;
 
     return Padding(
       padding: const EdgeInsets.all(4.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: backgroundColor, borderRadius: BorderRadius.circular(8)),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: todo.completed,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(4),
+      child: GestureDetector(
+        onTap: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return Container(
+                height: size.height * .75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(32),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Text(
+                          todo.task.toUpperCase(),
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
                     ),
-                    activeColor: Colors.black,
-                    checkColor: Colors.black,
-                    onChanged: (value) {
-                      final newTodo = todo.copyWith(completed: value);
-                      final todoProvider = context.read<TodoProvider>();
-
-                      todoProvider.update(newTodo);
-                    },
                   ),
-                  Text(
-                    todo.task,
-                    style: TextStyle(color: textColor),
-                  ),
-                ],
-              ),
-              FilledButton(
-                style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.all(backgroundColor),
-                    overlayColor:
-                        WidgetStatePropertyAll(textColor.withAlpha(75))),
-                onPressed: () {
-                  final todoProvider = context.read<TodoProvider>();
+                ),
+              );
+            },
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+              color: backgroundColor, borderRadius: BorderRadius.circular(8)),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Checkbox(
+                      value: todo.completed,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4),
+                          side: BorderSide(color: Colors.red)),
+                      activeColor: Colors.white,
+                      checkColor: Colors.black,
+                      onChanged: (value) {
+                        final newTodo = todo.copyWith(completed: value);
+                        final todoProvider = context.read<TodoProvider>();
 
-                  todoProvider.delete(todo);
-                },
-                child: Icon(Icons.delete_rounded, color: textColor),
-              )
-            ],
+                        todoProvider.update(newTodo);
+                      },
+                    ),
+                    Text(
+                      todo.task,
+                      style: TextStyle(
+                          color: textColor,
+                          decoration: todo.completed
+                              ? TextDecoration.lineThrough
+                              : null),
+                    ),
+                  ],
+                ),
+                FilledButton(
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.all(backgroundColor),
+                      overlayColor:
+                          WidgetStatePropertyAll(textColor.withAlpha(75))),
+                  onPressed: () {
+                    final todoProvider = context.read<TodoProvider>();
+
+                    todoProvider.delete(todo);
+                  },
+                  child: Icon(Icons.delete_rounded, color: textColor),
+                )
+              ],
+            ),
           ),
         ),
       ),
