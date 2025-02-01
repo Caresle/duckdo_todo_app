8c19ca1f9edb0bcde6c448c1b6f16f00448dc331
Caresle <carabesle@gmail.com>
2025-01-31 20:12:56 -0600

    feat(settings): add basic ui for section

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:duckdo_todo/screens/settings/row_item.dart';
 import 'package:duckdo_todo/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,19 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(226, 232, 240, 1),
-      body: Center(
-        child: Text('welcome to settings'),
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(226, 232, 240, 1),
+        title: Text('Settings'),
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            RowItem(icon: Icons.wb_sunny_rounded, text: 'Dark Theme'),
+            RowItem(icon: Icons.notifications_rounded, text: 'Reminders'),
+          ],
+        ),
       ),
       bottomNavigationBar: BottomNavigation(selectedItem: 1),
     );
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -2,3 +2,4 @@ export 'todo_card.dart';
 export './shared/bottom_navigation.dart';
 export 'add_todo.dart';
 export 'todo_list.dart';
+export './shared/custom_switch.dart';
